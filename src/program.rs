9a78100f32e2250dd2269cc079b51294110d3948
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2018-08-15 22:39:51 +0200

    Write stats to file

--- src/program.rs
+++ src/program.rs
@@ -1,9 +1,14 @@
 use std::env::Args;
 use std::error::Error;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::sync::mpsc;
 use std::thread;
 
-use time::{get_stats, Average};
+use chrono::offset::Local;
+
+use time::{get_stats, Average, Stats};
 pub struct Config {
     pub url: String,
     pub iterations: i32,
@@ -30,12 +35,32 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut stats = get_stats(&config.url)?;
 
+    let (file_tx, rx) = mpsc::channel::<Stats>();
+    thread::spawn(|| {
+        let mut file = OpenOptions::new()
+            .append(true)
+            .open("drift.csv")
+            .unwrap_or_else(|_| {
+                let mut file = File::create("drift.csv").expect("file: couldn't create file");
+
+                file.write(b"timestamp,offset,delay\n")
+                    .expect("file: couldn't write header in file");
+
+                file
+            });
+
+        for data in rx {
+            file.write(format!("{},{}\n", Local::now(), data).as_bytes())
+                .expect("file: couldn't write to file");
+        }
+    });
+
     let (tx, rx) = mpsc::channel();
     thread::spawn(move || {
-        println!("thread: starting, {} iters", config.iterations);
+        println!("ntp: starting, {} iters", config.iterations);
         for _ in 1..config.iterations {
             if let Ok(current) = get_stats(&config.url) {
-                tx.send(current).expect("couldn't send value");
+                tx.send(current).expect("ntp: couldn't send sample");
             }
         }
     });
@@ -44,6 +69,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     for sample in rx {
         stats.add_sample(&sample);
         println!("recv: {:?}", sample);
+        file_tx.send(sample).expect("main: couldn't send sample");
         iterations += 1;
     }
 
